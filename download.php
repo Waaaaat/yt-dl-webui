6a3b4500001d5f0f0ced21c4dd51b509ea433174
Janic Voser <53849986+Waaaaat@users.noreply.github.com>
2020-07-24 16:35:49 +0200

    Update download.php

--- download.php
+++ download.php
@@ -6,9 +6,11 @@
 # NonCopyright 2020 Janic Voser, Mettmenstetten, Switzerland.  All rights reserved.
 # YouTube-dl-WebUI Version 0.2
 #
-# Needed software:  - YOUTUBE-DL
-#                   - FFMPEG
+# Needed software:  - youtube-dl
+#                   - ffmpeg
 #                   - atomicparsley
+#
+# Additional Notes: - Give all needed Rights to the user www-data (webserveruser)
 #
 # Date: 09/07/2020
 -->
@@ -53,45 +55,45 @@
 
 
     <?php //start the PHP (Serversite) Section
+      $DOMAIN = "webtest.server-core.ch"; //Defines the Domain
       $WEBROOT = "/var/www/webtests"; //Defines The Websites Webroot
-      $FDL = "/var/www/webtests/Downloads"; //Defines The Download Folder for the Songs
-      $FODL = "/var/www/webtests/Downloads/old"; //Defines Where To Store the Downloaded Songs afterwards.
+      $FDL = "$WEBROOT/Downloads"; //Defines The Download Folder for the Songs
+      $DESTINATION ="$WEBROOT/Downloads/media"; //Defines The Download Folder for the Media
+      $FODL = "$WEBROOT/Downloads/old"; //Defines Where To Store the Downloaded Songs afterwards.
 
 
-      $DLParameters = "-i --no-playlist --embed-thumbnail --merge-output-format mp4";
+      $DLParameters = "-i --no-playlist --embed-thumbnail --merge-output-format mp4"; //Defines the Download Parameters
       $DLFormat = "x"; //mp3 or mp4 (audio or video) + --audio-format or video-format
       $DLURL = "URL-not-Provided"; //Enter the URL
       $GETFILE ="No File Found";
 
       //Get the Variables
-      if(isset($_GET['sendit'])){
-        $DLURL = $_GET['URL'];
-        $VoAF = $_GET['VoAF'];
+      if(isset($_GET['sendit'])){ //When button Presset get the variable
+        $DLURL = $_GET['URL']; //Get the variable
+        $VoAF = $_GET['VoAF']; //Get the variable
 
-        switch ($VoAF) {
-          case 'MP3':
-            $DLFormat = "-f 'bestaudio[ext=m4a]/bestvideo+bestaudio'";
+        switch ($VoAF) { //Check witch format to use
+          case 'MP3': //In case of MP3 selected use following
+            $DLFormat = "-f 'bestaudio[ext=m4a]/bestvideo+bestaudio'"; //Audio Format
             break;
-          case 'MP4':
-            $DLFormat = "-f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio'";
+          case 'MP4'://In case of MP4 selected use following
+            $DLFormat = "-f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio'";//Video Format
             break;
+          }
 
-
-
-        system("cd $FDL && youtube-dl $DLParameters $DLFormat $DLURL");
+        exec("cd $FDL && youtube-dl $DLParameters $DLFormat $DLURL");
         echo "<br>";
-        $GETFILE = passthru ("cd $FDL && ls *.m*");
-        echo "$GETFILE"; // Remove in the Final version
+        $GETFILE = system ("cd $FDL && ls *.m*");        //Get the Filename
+        $wgeturl = "https://$DOMAIN/Downloads/old/$GETFILE";
+        echo ("<a href=\"$wgeturl\" download>Download File to this Computer</a>");
+        echo "<br>";
+        exec ("cp $FDL/$GETFILE $FODL/$GETFILE");
+        exec ("mv $FDL/$GETFILE $DESTINATION/$GETFILE");
         echo "<br>"; // Remove in the Final version
 
-        //Get the Filename
 
         exec("cd $WEBROOT"); //Go back to the Webroot - Remove in the Final version replace
-        echo "<br>";  // Remove in the Final version replace with css
-        echo "<br>"; // Remove in the Final version
-        echo "youtube-dl $DLParameters $DLFormat $DLURL";  // Remove in the Final version replace with Download Shell
       }
-    }
     ?> <!-- End the PHP (Serversite) Section -->
 
   </center> <!-- Delete in Final Version - Replace with css -->
