d0d077d6ab498928e3360ee3d269cfbe41d91b2d
Janic Voser <53849986+Waaaaat@users.noreply.github.com>
2020-09-19 00:28:59 +0200

    Out Sourced PHP-processing

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 # Software isn't working right now:
 #
 # NonCopyright 2020 Janic Voser, Mettmenstetten, Switzerland.  All rights reserved.
-# YouTube-dl-WebUI Version 0.4
+# YouTube-dl-WebUI Version 0.5
 #
 # Needed software:  - youtube-dl
 #                   - ffmpeg
@@ -12,7 +12,7 @@
 #
 # Additional Notes: - Give all needed Rights to the user www-data (webserveruser)
 #
-# Date: 27/08/2020
+# Date: 18/09/2020
 -->
 
 <html>
@@ -62,85 +62,14 @@
 
       <br> <!-- Delete in Final Version - Replace with css -->
       <div>
-        <button name="sendit" value="sendit" type="submit">SEND IT !</button> 
+        <button name="sendit" value="sendit" type="submit">SEND IT !</button>
         <button name="downloadit" value="downloadit" type="submit">DOWNLOAD IT !</button>
       </div>
     </form> <!-- End the Form -->
 
   </div> <!-- End the Main Div -->
+<?php include 'php-processing.php' ?> <!-- Include the PHP Code -->
 
-  <?php //start the PHP (Serversite) Section
-        $DOMAIN = "ytdl.yourdomain.com"; //Defines the Domain
-        $WEBROOT = "/var/www/ytdl"; //Defines The Websites Webroot
-        $FDL = "$WEBROOT/Downloads"; //Defines The Download Folder for the Songs
-        $DESTINATION ="/home/emby/music"; //Defines The Download Folder for the Media
-        $FODL = "$WEBROOT/Downloads/old"; //Defines Where To Store the Downloaded Songs afterwards.
-        
-        $DLParameters = "-i --no-playlist --embed-thumbnail -o '%(title)s.%(ext)s' --merge-output-format mp4"; //Defines the Download Parameters
-        $DLFormat = "x"; //mp3 or mp4 (audio or video) + --audio-format or video-format
-        $DLURL = "URL-not-Provided"; //Enter the URL
-        $GETFILE ="No File Found";
-        
-        exec ("mkdir -p $FODL");
-
-        //Get the Variables
-        if(isset($_GET['sendit'])){ //When button Presset get the variable
-          $DLURL = $_GET['URL']; //Get the variable
-          $VoAF = $_GET['VoAF']; //Get the variable
-          
-          switch ($VoAF) { //Check witch format to use
-            case 'MP3': //In case of MP3 selected use following
-              $DLFormat = "-f 'bestaudio[ext=m4a]/bestvideo+bestaudio'"; //Audio Format
-            break;
-            case 'MP4'://In case of MP4 selected use following
-              $DLFormat = "-f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio'";//Video Format
-            break;
-          }
-          //echo ("youtube-dl $DLParameters $DLFormat $DLURL");
-          echo "<div id=phpbox class=box>";
-          exec("cd $FDL && youtube-dl $DLParameters $DLFormat $DLURL");
-          echo "<br>";
-          $GETFILE = system ("cd $FDL && ls *.m*");        //Get the Filename
-          $wgeturl = "https://$DOMAIN/Downloads/old/$GETFILE";
-          echo ("<a href=\"$wgeturl\" download>Download File to this Computer</a>");
-          echo "<br>";
-          exec ("cp $FDL/$GETFILE $FODL/$GETFILE");
-          exec ("mv $FDL/$GETFILE $DESTINATION/$GETFILE");
-          exec("cd $FDL && rm -f *.m* && rm -f *.webp");
-        
-          exec("cd $WEBROOT"); //Go back to the Webroot - Remove in the Final version replace
-          echo "</div>";
-        }
-        if(isset($_GET['downloadit'])){ //When button Presset get the variable
-          $DLURL = $_GET['URL']; //Get the variable
-          $VoAF = $_GET['VoAF']; //Get the variable
-          
-          switch ($VoAF) { //Check witch format to use
-            case 'MP3': //In case of MP3 selected use following
-              $DLFormat = "-f 'bestaudio[ext=m4a]/bestvideo+bestaudio'"; //Audio Format
-            break;
-            case 'MP4'://In case of MP4 selected use following
-              $DLFormat = "-f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio'";//Video Format
-            break;
-          }
-          //echo ("youtube-dl $DLParameters $DLFormat $DLURL");
-          echo "<div id=phpbox class=box>";
-          exec("cd $FDL && youtube-dl $DLParameters $DLFormat $DLURL");
-          echo "<br>";
-          $GETFILE = system ("cd $FDL && ls *.m*");        //Get the Filename
-          $wgeturl = "https://$DOMAIN/Downloads/old/$GETFILE";
-          echo ("<a href=\"$wgeturl\" download>Download File to this Computer</a>");
-          echo "<br>";
-          echo "$sendtonas";
-          exec ("cp $FDL/$GETFILE $FODL/$GETFILE");
-          exec ("rm -f $FDL/$GETFILE");
-          exec("cd $FDL && rm -f *.m* && rm -f *.webp");
-          
-          exec("cd $WEBROOT"); //Go back to the Webroot - Remove in the Final version replace
-          echo "</div>";
-        }
-      ?>
-  <!-- End the PHP (Serversite) Section -->
 </body><!-- End the Bodysection -->
 
 </html><!-- End the HTML-Section -->
